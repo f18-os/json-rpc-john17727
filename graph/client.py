c08a0e64328ba0ea2950117c5d5de6828233fc80
john17727 <justanotherone17727@gmail.com>
2018-11-09 14:37:46 -0700

    Starter Files

---
+++ graph/client.py
@@ -0,0 +1,25 @@
+import socket
+from node import *
+from bsonrpc import JSONRpc
+from bsonrpc.exceptions import FramingError
+from bsonrpc.framing import (
+	JSONFramingNetstring, JSONFramingNone, JSONFramingRFC7464)
+
+leaf1 = node("leaf1")
+leaf2 = node("leaf2")
+
+root = node("root", [leaf1, leaf1, leaf2])
+
+root.show()
+
+# Cut-the-corners TCP Client:
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 50001))
+
+rpc = JSONRpc(s,framing_cls=JSONFramingNone)
+server = rpc.get_peer_proxy()
+# Execute in server:
+root = server.increment(root)
+root.show()
+
+rpc.close() # Closes the socket 's' also
---
+++ graph/server.py
@@ -0,0 +1,26 @@
+import socket
+from node import *
+from bsonrpc import JSONRpc
+from bsonrpc import request, service_class
+from bsonrpc.exceptions import FramingError
+from bsonrpc.framing import (
+	JSONFramingNetstring, JSONFramingNone, JSONFramingRFC7464)
+
+
+# Class providing functions for the client to use:
+@service_class
+class ServerServices(object):
+
+  @request
+  def increment(self, root):
+    return increment(root)
+
+# Quick-and-dirty TCP Server:
+ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ss.bind(('localhost', 50001))
+ss.listen(10)
+
+while True:
+  s, _ = ss.accept()
+  # JSONRpc object spawns internal thread to serve the connection.
+  JSONRpc(s, ServerServices(),framing_cls=JSONFramingNone)
