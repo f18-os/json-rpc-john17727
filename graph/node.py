1d80c915a4bcf0b4c5e755d6e4d4dfc173a6ba26
john17727 <justanotherone17727@gmail.com>
2018-11-09 16:54:47 -0700

    Using dictionary

--- graph/node.py
+++ graph/node.py
@@ -3,6 +3,8 @@ class node:
         self.name = name
         self.children = children
         self.val = 0
+    def value(val):
+        self.val = val
     def show(self, level=0):
         print("%s%s val=%d:" % (level*"  ", self.name, self.val))
         for c in self.children: 
@@ -13,3 +15,15 @@ def increment(graph):
     for c in graph.children:
         increment(c)
 
+def toDict(graph):
+    dict = {}
+    count = 0
+    dict[graph.name] = graph.val
+    for c in graph.children:
+        if(c.name in dict):
+            name = c.name + "_" + str(count)
+            count += 1
+            dict[name] = c.val
+        else:
+            dict[c.name] = c.val
+    return dict
